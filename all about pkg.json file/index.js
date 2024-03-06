4d164c945e0696cab0721b43b2e5d67a91ad8cc1
Abhishek Mishra <mishra.abhishek01@hotmail.com>
2024-03-06 21:39:38 +0530

    delete node module

--- all about pkg.json file/index.js
+++ all about pkg.json file/index.js
@@ -1,8 +1,15 @@
-var colors = require('colors')
-console.log('hello'.red);
-console.log('hello'.bgBlue);
+// var colors = require('colors')
+// console.log('hello'.red);
+// console.log('hello'.bgBlue);
 
 // pkg - https://www.npmjs.com/package/colors
 
 
 // node js is single threded means one time pr ek hi command run hogi 
+
+
+
+// import chalk from 'chalk';
+var chalk = require('chalk')
+
+console.log(chalk.blue('Hello world!'));
