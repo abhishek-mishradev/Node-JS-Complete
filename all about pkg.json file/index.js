95ed07e9cb4da16f27b528d58d84cc218b225f7d
Abhishek Mishra <mishra.abhishek01@hotmail.com>
2024-03-06 21:13:35 +0530

    pkg.json

---
+++ all about pkg.json file/index.js
@@ -0,0 +1,8 @@
+var colors = require('colors')
+console.log('hello'.red);
+console.log('hello'.bgBlue);
+
+// pkg - https://www.npmjs.com/package/colors
+
+
+// node js is single threded means one time pr ek hi command run hogi 
