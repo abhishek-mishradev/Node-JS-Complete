4340c600f52c0ad3e74b5578fecbe56912d89aaa
Abhishek Mishra <mishra.abhishek01@hotmail.com>
2024-03-07 00:07:33 +0530

    simple api

---
+++ simple api/data.js
@@ -0,0 +1,7 @@
+const data= [
+    {name: "abhishek mishra", email:"a@gmail.com"},
+    {name: "atharv mishra", email:"b@gmail.com"},
+    {name: "ayushi mishra", email:"bb@gmail.com"}
+]
+
+module.exports=data;
