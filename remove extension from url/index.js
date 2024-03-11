60d2e14ceffb79040ab5145d7e6ffb9b2cc0f2e9
Abhishek Mishra <mishra.abhishek01@hotmail.com>
2024-03-11 20:34:26 +0530

    html page and 404 page creation

---
+++ remove extension from url/index.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const path= require("path");
+
+const app= express();
+
+const publicPath= path.join(__dirname, 'public')
+
+app.get("", (_, resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+
+});
+
+app.get("/about", (_, resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+
+});
+
+app.get("*", (_, resp)=>{
+    resp.sendFile(`${publicPath}/pageNotFound.html`)
+
+});
+
+
+app.listen(5000)
